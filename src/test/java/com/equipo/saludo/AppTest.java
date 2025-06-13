2ad906afa0b0b4bc6c18e61d6fd9018cfe1162b7
EriickT1 <eriick1324@gmail.com>
2025-06-12 23:02:41 -0400

    Probando deploy

--- src/test/java/com/equipo/saludo/AppTest.java
+++ src/test/java/com/equipo/saludo/AppTest.java
@@ -2,7 +2,7 @@ package com.equipo.saludo;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+// Probando deploy
 public class AppTest {
     @Test
     public void testSaludo() {
